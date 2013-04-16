ace962c44a98eb4551111f960be2b3f43d735bf7
Giacomo Bagnoli <gbagnoli@gmail.com>
2013-04-16 19:52:12 +0100

    Initial code structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+requires = ["openphoto"]
+try:
+    from setuptools import setup
+    kw = {
+        "entry_points": """
+[console_scripts]
+    openphoto-downloader = openphoto_utils.downloader:main
+""",
+        "zip_safe": False,
+        "install_requires": requires
+    }
+except ImportError:
+    print("Using distutils")
+    from distutils.core import setup
+    kw = {
+        "scripts": ['scripts/openphoto-downloader'],
+        "requires": "requires"
+    }
+
+setup(name="openphoto-utils",
+      version="0.1",
+      description="Utils for openphoto",
+      author="Giacomo Bagnoli",
+      author_email="gbagnoli@gmail.com",
+      packages=["openphoto_utils"],
+      **kw
+)
+
+
+
