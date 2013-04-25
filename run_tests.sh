f7ddee7060d2ce5602aadf0725a7d0cc4a28742c
Giacomo Bagnoli <gbagnoli@gmail.com>
2013-04-25 18:57:23 +0100

    Added support for python 3.2 in travisCI

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-echo $(python --version 2>&1) | grep -q "3.3"
+version=$(python --version 2>&1)
 
+echo $version | grep -q "3.3"
 if [ $? -eq 0 ];then
     python -m unittest discover -s tests
 else
-    pip install argparse unittest2 mock --use-mirrors 2>&1 > /dev/null
-    unit2 discover -s tests
+    echo $version | grep -q "3.2"
+    if [ $? -eq 0 ]; then
+        pip install mock --use-mirrors 2>&1 >/dev/null
+        python -m unittest discover -s tests
+    else
+        pip install argparse unittest2 mock --use-mirrors 2>&1 > /dev/null
+        unit2 discover -s tests
+    fi
 fi
