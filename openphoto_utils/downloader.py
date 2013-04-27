6f66eb77adc3478457b921102de7e65a472c405e
Giacomo Bagnoli <bagnoli@amazon.com>
2013-04-27 20:58:53 +0100

    First, quick, cheap version of the downloader using pyopenphoto

--- openphoto_utils/downloader.py
+++ openphoto_utils/downloader.py
@@ -4,6 +4,9 @@ import argparse
 import logging
 import os
 import shutil
+from openphoto import (Album,
+                       Photo,
+                       Tag)
 from .config import Config
 from .main import run
 
@@ -39,29 +42,68 @@ def download_photos(config, destination, cache=None):
         raise OSError("No such directory '%s'" % (destination))
 
     photo_dir = os.path.join(destination, ".photos")
+    try:
+        os.mkdir(photo_dir)
+    except OSError:
+        pass
+
     if cache:
         cache = populate_cache(cache)
     else:
         cache = {}
 
-    photos = client.photos.list()
-    urls = []
-    for photo in photos:
+    for photo in Photo.all(client):
         if photo.hash in cache:
             shutil.copy(cache[photo.hash], destination)
         else:
-            urls.append(photo.pathDownload)
+            print("Downloading photo %s" % (photo.id))
+            photo_path = os.path.join(photo_dir, photo.id)
+            if os.path.isfile(photo_path):
+                continue
+            try:
+                photo.download(photo_path)
+            except Exception as e:
+                print ("Error downloading photo %s: %s" % (photo.id, e))
 
-    failed = set()
-    for u in urls:
+    for album in Album.all(client):
+        album_dir = os.path.join(destination, album.name)
+        print("Creating links for album %s" % (album.name))
+        try:
+            os.mkdir(album_dir)
+        except OSError:
+            pass
 
-        if r.status_code != 200:
-            failed.add(r)
-        else:
-            print(r)
+        for photo in album.photos():
+            photo_path = os.path.join(photo_dir, photo.id)
+            if not os.path.isfile(photo_path):
+                continue
+            ext = os.path.splitext(photo.filename_original)[-1].lower()
+            dest = os.path.join(album_dir, "%s%s" % (photo.id, ext))
+            if not os.path.exists(dest):
+                os.link(photo_path, dest)
 
-    print failed
+    tags_dir = os.path.join(destination, "tags")
+    try:
+        os.mkdir(tags_dir)
+    except OSError:
+        pass
 
+    for tag in Tag.all(client):
+        tag_dir = os.path.join(tags_dir, tag.id)
+        print("Creating links for tag %s" % (tag.id))
+        try:
+            os.mkdir(tag_dir)
+        except OSError:
+            pass
+
+        for photo in tag.photos():
+            photo_path = os.path.join(photo_dir, photo.id)
+            if not os.path.isfile(photo_path):
+                continue
+            ext = os.path.splitext(photo.filename_original)[-1]
+            dest = os.path.join(tag_dir, "%s%s" % (photo.id, ext))
+            if not os.path.exists(dest):
+                os.link(photo_path, dest)
 
 
 if __name__ == '__main__':
--- openphoto_utils/main.py
+++ openphoto_utils/main.py
@@ -1,6 +1,4 @@
 import logging
-import socket
-import httplib2
 
 log = logging.getLogger(__name__)
 
@@ -12,12 +10,6 @@ def run(fun, config,  *args, **kwargs):
 
     except (OSError,) as e:
         log.critical(e)
-
-    except socket.error as e:
-        log.critical("Cannot connect to the remote API at %s: %s", config.api.host, e)
-
-    except httplib2.ServerNotFoundError as e:
-        log.critical("Cannot resolve name for remote API at %s: %s", config.api.host, e)
 
     except Exception as e:
         log.exception("Unknown error [%s]", type(e))
